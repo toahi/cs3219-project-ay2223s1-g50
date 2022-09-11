3d3d0c71f11444c0c0b57d77345f7d27b0def0f0
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2022-09-11 13:34:20 +0800

    use mongo memory server

--- user-service/model/repository.js
+++ user-service/model/repository.js
@@ -3,10 +3,13 @@ import 'dotenv/config'
 
 //Set up mongoose connection
 import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
-let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
+// use memory server for now for ease of dev
+const mongoDB = await MongoMemoryServer.create();
+// let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(mongoDB.getUri(), { useNewUrlParser: true , useUnifiedTopology: true});
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
