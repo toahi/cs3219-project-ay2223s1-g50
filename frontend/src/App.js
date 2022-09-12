9ccee32219c1ff615097419d0c2ec07f7bf7859f
toahi <59340399+toahi@users.noreply.github.com>
2022-09-12 16:45:38 +0800

    Revert "[do not merge] Add login and navbar, update signup"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,15 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import SignupPage from './components/SignupPage';
-import LoginPage from "./components/LoginPage";
 import {Box} from "@mui/material";
-import NavBar from "./components/layouts/Navbar";
 
 function App() {
     return (
         <div className="App">
-            <NavBar />
             <Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
                 <Router>
                     <Routes>
                         <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
                         <Route path="/signup" element={<SignupPage/>}/>
-                        <Route path="/login" element={<LoginPage/>}/>
                     </Routes>
                 </Router>
             </Box>
