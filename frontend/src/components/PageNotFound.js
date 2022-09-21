d1b6901268c970ecfd113aa66c49e857257f244c
cwq2326 <e0556260@u.nus.edu>
2022-09-21 15:24:22 +0800

    Improve styling of not found page

--- frontend/src/components/PageNotFound.js
+++ frontend/src/components/PageNotFound.js
@@ -1,8 +1,11 @@
-// TODO 
-// Make this page more beautiful
+import classes from "./PageNotFound.module.css"
 
 const PageNotFound = () => {
-    return <h1>PAGE NOT FOUND!</h1>
+    return <div className={classes.container}>
+        <h1 className={classes.title}>404</h1>
+        <h2>Page not found</h2>
+        <h3>The page you requested could not be found</h3>
+    </div>
 }
 
 export default PageNotFound
