f559cfa3e72ddbbd633cb8ddf58335b969ed3b24
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2022-09-26 21:02:48 +0800

    Fix redis

--- user-service/redis.js
+++ user-service/redis.js
@@ -6,10 +6,11 @@ import { RedisMemoryServer } from 'redis-memory-server';
 // use Redis memory server for now for ease of dev
 const redisServer = new RedisMemoryServer();
 
+// fix lint issue
 const host = await redisServer.getHost();
 const port = await redisServer.getPort();
 
-const redis = new Redis(1, 1);
+const redis = new Redis(host, port);
 redis.on('error', (err) => console.log('Redis Client Error', err));
 
 export default redis;
