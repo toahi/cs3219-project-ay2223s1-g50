4bec0a22382cf868f44df721fda6bcf81a1f4a91
cwq2326 <e0556260@u.nus.edu>
2022-10-04 22:26:56 +0800

    Add href to about in home

--- frontend/src/components/home/Hero.js
+++ frontend/src/components/home/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
                 <h1 className={classes.title}>Get hired at your dream job!</h1>
                 <h3 className={classes.description}>PeerPrep prepares you for online technical interview by increasing your technical competence and confident so that you can land your dream job.</h3>
             <div className={classes["button-container"]}>
-                <Button className={classes["button-learn-more"]} variant="text" style={{border: "black solid 1px", backgroundColor: "#FFFFFF", fontSize: "1.3rem", width: "45%", height: "4rem"}}>LEARN MORE</Button>
+                <Button className={classes["button-learn-more"]} variant="text" href="/about" style={{border: "black solid 1px", backgroundColor: "#FFFFFF", fontSize: "1.3rem", width: "45%", height: "4rem"}}>LEARN MORE</Button>
                 <Button className={classes["button-sign-up"]} variant="contained" href="/signup" style={{fontSize: "1.3rem", width: "45%", height: "4rem"}}>SIGN UP</Button>
             </div>
             </div>
