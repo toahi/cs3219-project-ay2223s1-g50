573b1f276f959516dd392580b2c6810b48160a4c
cwq2326 <e0556260@u.nus.edu>
2022-10-04 16:50:35 +0800

    Add buttons styling

--- frontend/src/components/home/Hero.js
+++ frontend/src/components/home/Hero.js
@@ -1,6 +1,5 @@
 import classes from "./Hero.module.css";
-import Button from 'react-bootstrap/Button';
-
+import { Button } from "@mui/material";
 const Hero = () => {
     return (
         <div className={classes.container}>
@@ -8,8 +7,8 @@ const Hero = () => {
                 <h1 className={classes.title}>Get hired at your dream job!</h1>
                 <h3 className={classes.description}>PeerPrep prepares you for online technical interview by increasing your technical competence and confident so that you can land your dream job.</h3>
             <div className={classes["button-container"]}>
-                <Button className={classes["button-learn-more"]} variant="primary">LEARN MORE</Button>{' '}
-                <Button className={classes["button-sign-up"]} variant="light">SIGN UP</Button>{' '}
+                <Button className={classes["button-learn-more"]} variant="contained" style={{fontSize: "1.3rem", width: "45%", height: "4rem"}}>LEARN MORE</Button>
+                <Button className={classes["button-sign-up"]} variant="text" href="/signup" style={{backgroundColor: "#FFFFFF", fontSize: "1.3rem", width: "45%", height: "4rem"}}>SIGN UP</Button>
             </div>
             </div>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkqTCEe8NPl1pHhHt1DFy1OMtldq3P_RQ0qA&usqp=CAU" />
