ab92259b0ba5c8e86bf03a2fb855a4ce53df1c84
cwq2326 <e0556260@u.nus.edu>
2022-10-04 23:44:48 +0800

    Fix loading page style

--- frontend/src/components/LoadingPage.js
+++ frontend/src/components/LoadingPage.js
@@ -3,11 +3,11 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 const LoadingPage = () => {
     return (
-        <div data-testid="loading-page" style={{ margin: 'auto', paddingTop: '15rem'}}>
-        <Box sx={{ display: 'flex' }}>
-            <h1>Loading...</h1>
-            <CircularProgress />
-        </Box>
+        <div data-testid="loading-page">
+            <Box sx={{ display: "flex", justifyContent: "center", marginTop: "20%"}}>
+                <h1>Loading...</h1>
+                <CircularProgress />
+            </Box>
         </div>
     )
 }
