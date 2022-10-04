83fa3ce38ba549cfe4e290fd6323da68418fe004
cwq2326 <e0556260@u.nus.edu>
2022-10-04 22:27:58 +0800

    Add hero to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import SignupPage from './components/SignupPage';
 import LoginPage from "./components/LoginPage";
-import {Box} from "@mui/material";
 import NavBar from "./components/layouts/Navbar";
 import DashBoard from "./components/Dashboard";
 import RequireAuth from "./components/protected-routes/RequireAuth";
 import UserContextProvider from "./components/context/user-context";
 import PageNotFound from "./components/PageNotFound";
 import LoadingPage from "./components/LoadingPage";
-import Onboarding from "./components/home/Onboarding";
+import Hero from "./components/home/Hero";
 import Profile from "./components/Profile";
 import Interview from "./components/Interview";
 import AboutPage from "./components/home/AboutPage";
-
-const styles = {
-  root: {
-    padding: "10px"
-  }
-};
 
 function App() {
 
@@ -29,7 +22,7 @@ function App() {
                 <Router>
                     <Routes>
                         {/* <Route element={<ExistingAuth />}> */}
-                            <Route exact path="/" element={<Onboarding />} />
+                            <Route exact path="/" element={<Hero />} />
                             <Route path="/signup" element={<SignupPage />} />
                             <Route path="/login" element={<LoginPage />} />
                         {/* </Route> */}
